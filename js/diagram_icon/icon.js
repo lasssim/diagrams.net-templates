8ade96a123dfd9a595c61871779a39bf297e077c
Simon Lasselsberger <simon@lasssim.com>
2022-10-07 14:17:22 +0200

    Fix on click handler

--- js/diagram_icon/icon.js
+++ js/diagram_icon/icon.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-  $("img[src*='drawio']").each(function() {
-    var src = $(this).attr("src");
+function enable_automatic_drawio_edit_icon(on_click) {
+  $(document).ready(function() {
+    $("img[src*='drawio']").each(function() {
+      var src = $(this).attr("src");
 
-    $(this).wrap('<div class="diagram_icon_wrapper"></div>');
-    $(this).after(`<a class="diagram_icon" href="" target="_blank"></a>`);
+      $(this).wrap('<div class="diagram_icon_wrapper"></div>');
+      $(this).after(`<a class="diagram_icon" href="" target="_blank"></a>`);
 
-    diagram_button({
-      target_id: $(this).next(),
-      open_url: src
-    }, function() {
-      console.log("clicked");
-    }
-    );
+      diagram_button({
+          target_id: $(this).next(),
+          open_url: src,
+          on_click: function() {
+            if (on_click) { on_click(src); }
+          }
+        } 
+      );
 
-
+    });
   });
-});
+};
